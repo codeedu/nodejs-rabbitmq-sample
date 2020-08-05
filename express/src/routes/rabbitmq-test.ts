6bd171a436ab87873cdb106a3812fae28d71d0b7
Luiz Carlos <argentinaluiz@gmail.com>
2020-08-05 08:55:08 -0300

    Sample without rabbitmq

--- express/src/routes/rabbitmq-test.ts
+++ express/src/routes/rabbitmq-test.ts
@@ -1,9 +1,8 @@
 import express from 'express';
 const router = express.Router();
 
-router.post('/express', function(req, res, next) {
-  console.log(req.body);
-  res.send('respond with a resource');
+router.post('/express', async function(req, res, next) {
+  res.send(req.body);
 });
 
 export default router;
